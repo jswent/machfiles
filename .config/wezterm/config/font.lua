c64ae08e2ee4e6fbe6b671002b69f3e1a5654ec6
James Swent <jake@osprey21.com>
2024-09-23 02:49:17 +0100

    feat(font): Add font fallback specification

--- .config/wezterm/config/font.lua
+++ .config/wezterm/config/font.lua
@@ -1,7 +1,18 @@
 local wezterm = require("wezterm")
+local utils = require("lua.utils")
 
-return {
-	font = wezterm.font("JetBrains Mono"),
-	font_size = 12,
+local function build_wt_font_config()
+	return {
+		font = wezterm.font_with_fallback({
+			"JetBrains Mono",
+			"Symbols Nerd Font",
+		}),
+		font_size = 12,
+	}
+end
+
+local C = {
 	adjust_window_size_when_changing_font_size = false,
 }
+
+return utils.deep_extend("force", C, build_wt_font_config())
