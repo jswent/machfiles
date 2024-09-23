db62e96c3e300fa48943b6777d27e394f6a9bb4b
James Swent <jake@osprey21.com>
2024-09-23 02:49:45 +0100

    feat(ui): Application-specific padding

--- .config/wezterm/config/ui.lua
+++ .config/wezterm/config/ui.lua
@@ -22,13 +22,48 @@ local function ui_for_appearance(appearance)
 	end
 end
 
-local C = {
-	window_padding = {
-		left = 20,
-		right = 10,
-		top = 20,
-		bottom = 10,
+-- Define a table of window_padding settings to be reused
+local padding = {
+	default = {
+		left = "1cell",
+		right = "1cell",
+		top = "0.5cell",
+		bottom = "0.5cell",
 	},
+	tui = {
+		left = 0,
+		right = 0,
+		top = "0.25cell",
+		bottom = 0,
+	},
+	none = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	},
+}
+
+-- Define a table of programs and their corresponding padding settings
+local program_padding = {
+	nvim = padding.tui,
+	yazi = padding.tui,
+}
+
+wezterm.on("update-status", function(window, pane)
+	local process_name = pane:get_foreground_process_name()
+	if process_name then
+		local padding_setting = program_padding[process_name:match("[^/]+$")]
+		if padding_setting then
+			window:set_config_overrides({ window_padding = padding_setting })
+		else
+			window:set_config_overrides({ window_padding = padding.default })
+		end
+	end
+end)
+
+local C = {
+	window_padding = padding.default,
 	default_cursor_style = "BlinkingBar",
 	window_decorations = "RESIZE",
 }
