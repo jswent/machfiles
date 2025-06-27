526110a2f8664ec68161e7b31edb46e595af95d5
James Swent <jake@osprey21.com>
2025-06-26 22:28:57 -0400

    feat: Add starship system theme support

--- scripts/onThemeChange.sh
+++ scripts/onThemeChange.sh
@@ -3,6 +3,7 @@
 PATH="$PATH:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/homebrew/bin"
 
 USER_DIR="/Users/jswent"
+MACHFILES_DIR="$USER_DIR/machfiles"
 CONFIG_DIR="$USER_DIR/.config"
 KITTY_DIR="$CONFIG_DIR/kitty"
 
@@ -48,6 +49,41 @@ update_nvim_appearance() {
   done
 }
 
+update_starship_theme() {
+  local new_theme="$1"
+  local file
+
+  if [[ -d "$MACHFILES_DIR" ]]; then
+    file="$MACHFILES_DIR/config/starship.toml"
+  else
+    file="$CONFIG_DIR/starship.toml"
+  fi
+
+  sed -i '' "s/^palette = \"[^\"]*\"/palette = \"$new_theme\"/" "$file"
+}
+
+update_starship_appearance() {
+  local appearance="$1"
+  local config_path="$CONFIG_DIR/starship.toml"
+
+  if [ ! -d "$MACHFILES_DIR" ]; then
+    return 1
+  fi
+
+  if [ ! -L "$config_path" ]; then
+    return 1
+  fi
+
+  echo "reached"
+  if [ "$appearance" = "light" ]; then
+    rm "$config_path"
+    ln -s "$MACHFILES_DIR/config/starship-light.toml" "$config_path"
+  elif [ "$appearance" = "dark" ]; then
+    rm "$config_path"
+    ln -s "$MACHFILES_DIR/config/starship-dark.toml" "$config_path"
+  fi
+}
+
 # Check that dark mode is received from service
 if [ -n "$DARKMODE" ]; then
   if [ "$DARKMODE" -eq 1 ]; then
@@ -55,11 +91,13 @@ if [ -n "$DARKMODE" ]; then
     update_kitty_theme "$KITTY_DIR/themes/solarized_custom.conf"
     # update_nvim_theme "tokyonight-moon"
     update_nvim_appearance "dark"
+    update_starship_appearance "dark"
   elif [ "$DARKMODE" -eq 0 ]; then
     echo "Switching to light mode"
     update_kitty_theme "$KITTY_DIR/themes/solarized_light.conf"
     # update_nvim_theme "tokyonight-day"
     update_nvim_appearance "light"
+    update_starship_appearance "light"
   else
     echo "Invalid DARKMODE value: $DARKMODE"
   fi
